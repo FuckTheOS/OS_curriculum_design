179bc0e5480e0cdb622f02103d90f4bb74e0bb07
morejarphone <morejarphone@gmail.com>
2017-07-09 17:45:54 +0800

    Update dir.cpp

--- OS_project/dir.cpp
+++ OS_project/dir.cpp
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "dir.h"
 #include "index.h"
-#include "file.h"
+
 dirBlock readDir (int id) {				//根据文件块id读取文件块信息
 	if (id >= DIRSIZE || id < 0) {
 		cout << "段错误！" << endl;
@@ -25,9 +25,24 @@ void writeDir (dirBlock db, int id){	//将目录块信息写入目录块
 	fout.close ();
 }
 
-int giveDirBlock (int dirType){			//分配新的目录块
-	return -1;
+int giveDirBlock (){					//分配新的目录块 
+	superNodeBlock sn = readSuperNode ();
+	if (sn.emptyDirBlock == -1) {		//目录块空间不足
+		return -1;
+	}
+	int res = sn.emptyDirBlock;
+	if (sn.emptyDirBlock == sn._emptyDirBlock) {	//目录块刚好用完
+		sn.emptyDirBlock = -1;
+		sn._emptyDirBlock = -1;
+	}
+	else {
+		dirBlcok db = readDir (sn.emptyDirBlock);	//读取空目录块信息
+		sn.emptyDirBlock = db.nextDirID;			//该块的下一块作为空目录块的首块
+	}
+	return res;
 }
+//参数表示目录块类型
+//如果分配成功返回目录块ID 否则返回-1
 
 bool checkDirName (string newDirName){	//检查目录名是否和当前其他目录冲突
 	dirBlock db = readDir (curDirID);
@@ -52,15 +67,17 @@ void showAllSonDir (){	//显示当前路径下所有子目录
 		cout << endl;
 		return ;
 	}
-	db = readDir (db.sonDirID);
+	indexBlock ib = readIndex (db.sonDirID);
+	db = readDir (ib.offsetID);
 	cout << db.dirName << " ";
 	while (db.nextDirID != -1) {
-		indexBlock ib = readIndex (db.nextDirID);
+		ib = readIndex (db.nextDirID);
 		db = readDir (ib.offsetID);
 		cout << db.dirName << " ";
 	}
 	cout << endl;
 }
+//当前路径直接用全局变量
 //按照a b.txt c d.cpp 的格式输出
 
 bool mkdir (string newDirName, string newDirMod) {	//在当前目录下创建子目录
@@ -78,6 +95,35 @@ bool mkdir (string newDirName, string newDirMod) {	//在当前目录下创建子
 		cout << "磁盘空间不足!" << endl;
 		return false;
 	}
+	indexBlock ib = readIndex (indexID);	//更新索引块信息
+	ib.used = 1;
+	ib.offsetID = dirID;
+	writeIndex (ib, indexID);
+
+	dirBlock db = readDir (curDirID);
+	if (db.sonDirID == -1) {
+		db.sonDirID = indexID;
+	}
+	else {
+		int tmp = db.sonDirID;
+		while (db.nextDirID != -1) {		//找到当前路径的最后一个目录 
+			db = readDir (db.nextDirID);
+		}
+		db.nextDirID = dirID;
+	}
+
+	db = readDir (dirID);
+	strcpy (db.fileName, newDirName.c_str ());
+	char dirOwner[20];	//目录创建者
+	db.dirSize = 0;
+	db.dirCreateTime;	//目录创建时间
+	db.dirChangeTime;	//目录上一次修改时间
+	db.type = 1;
+	db.textLocation = -1;
+	db.faDirID = curDirID;
+	db.sonDirID = -1;
+	db.nextDirID = -1;
+	db.used = 1;
 	return true;
 }
 
@@ -107,12 +153,11 @@ bool mkdirs (string newDirPath, string newDirMod){	//在当前目录下创建多
 
 bool gotoDir (string tarPath){			//跳转到新的目录
 	vector <string> path = pathPrase (tarPath);
-	dirBlock curDirBlock = readDir(curDirID);
-	int tmp_no=curDirID;//记录当前块的编号
-    for(auto op:path)
-        if(!visitPath(curDirBlock, op ,tmp_no)) return false;
-    curDirID = tmp_no;
-	return true;
+	dirBlock* curDirBlock = &readDir(curDirID);
+    	for(auto op:path)
+        	if(!visitPath(op)) return false;
+    	curDirID = curDirBlock - &readDir(0);
+		return true;
 }
 //参数是相对或绝对路径 需要路径解析自动机解析路径
 //跳转成功返回1 否则返回0
