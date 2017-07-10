1e7d41a11b2d1ade628c1ea3d9383d983db1fd6a
morejarphone <morejarphone@gmail.com>
2017-07-10 11:39:31 +0800

    Update file.h

--- OS_project/file.h
+++ OS_project/file.h
@@ -1,7 +1,6 @@
 #ifndef FILE_H_INCLUDED
 #define FILE_H_INCLUDED
 
-bool checkMod (int modType);	//用户在当前路径下对欲使用此权限
 fileBlock readFile (int id);	//根据文件块id读取文件内容
 //访盘，注意保护原有数据
 void writeFile (fileBlock db, int id);  	//将文件内容写入文件块
