d1566c81932b5114ff86f83b578fb6c30126387b
morejarphone <morejarphone@gmail.com>
2017-07-10 11:39:09 +0800

    Update file.cpp

--- OS_project/file.cpp
+++ OS_project/file.cpp
@@ -1,12 +1,6 @@
 #include "common.h"
 #include "file.h"
 
-bool checkMod (int modType) {	//用户在当前路径下对欲使用此权限
-	//用户和当前路径直接读取全局变量
-	//如果可以使用此权限返回true 否则flase
-	return true;
-}
-
 fileBlock readFile (int id){	//根据文件块id读取文件内容
 	fileBlcok fb;
 	ifstream fin (dis.c_str (), std::ios::binary);
